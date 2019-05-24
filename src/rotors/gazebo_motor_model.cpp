26906783b1af6a09f46cbd80ed1a656e575a089b
Kye Morton <pryre.dev@outlook.com>
2019-05-24 12:40:01 +1000

    Updated old RotorS code to match new codebase, added odom-triplet feedback

--- src/rotors/gazebo_motor_model.cpp
+++ src/rotors/gazebo_motor_model.cpp
@@ -162,74 +162,73 @@ void GazeboMotorModel::VelocityCallback(const std_msgs::Float64MultiArray::Const
 }
 
 void GazeboMotorModel::UpdateForcesAndMoments() {
-  motor_rot_vel_ = joint_->GetVelocity(0);
-  if (motor_rot_vel_ / (2 * M_PI) > 1 / (2 * sampling_time_)) {
-    gzerr << "Aliasing on motor [" << motor_number_ << "] might occur. Consider making smaller simulation time steps or raising the rotor_velocity_slowdown_sim_ param.\n";
-  }
-  double real_motor_velocity = motor_rot_vel_ * rotor_velocity_slowdown_sim_;
-  double force = real_motor_velocity * real_motor_velocity * motor_constant_;
+	motor_rot_vel_ = joint_->GetVelocity(0);
+	if (motor_rot_vel_ / (2 * M_PI) > 1 / (2 * sampling_time_)) {
+	gzerr << "Aliasing on motor [" << motor_number_
+		  << "] might occur. Consider making smaller simulation time "
+			 "steps or raising the rotor_velocity_slowdown_sim_ param.\n";
+	}
+	double real_motor_velocity =
+	  motor_rot_vel_ * rotor_velocity_slowdown_sim_;
+	// Get the direction of the rotor rotation.
+	int real_motor_velocity_sign =
+	  (real_motor_velocity > 0) - (real_motor_velocity < 0);
+	// Assuming symmetric propellers (or rotors) for the thrust calculation.
+	double thrust = turning_direction_ * real_motor_velocity_sign *
+				  real_motor_velocity * real_motor_velocity *
+				  motor_constant_;
 
-  // scale down force linearly with forward speed
-  // XXX this has to be modelled better
-  math::Vector3 body_velocity = link_->GetWorldLinearVel();
-  double vel = body_velocity.GetLength();
-  double scalar = 1 - vel / 25.0; // at 50 m/s the rotor will not produce any force anymore
-  scalar = math::clamp(scalar, 0.0, 1.0);
-  // Apply a force to the link.
-  link_->AddRelativeForce(math::Vector3(0, 0, force * scalar));
+	// Apply a force to the link.
+	link_->AddRelativeForce(ignition::math::Vector3d (0, 0, thrust));
 
-  // Forces from Philppe Martin's and Erwan Salaün's
-  // 2010 IEEE Conference on Robotics and Automation paper
-  // The True Role of Accelerometer Feedback in Quadrotor Control
-  // - \omega * \lambda_1 * V_A^{\perp}
-  math::Vector3 joint_axis = joint_->GetGlobalAxis(0);
-  //math::Vector3 body_velocity = link_->GetWorldLinearVel();
-  math::Vector3 body_velocity_perpendicular = body_velocity - (body_velocity * joint_axis) * joint_axis;
-  math::Vector3 air_drag = -std::abs(real_motor_velocity) * rotor_drag_coefficient_ * body_velocity_perpendicular;
-  // Apply air_drag to link.
-  link_->AddForce(air_drag);
-  // Moments
-  // Getting the parent link, such that the resulting torques can be applied to it.
-  physics::Link_V parent_links = link_->GetParentJointsLinks();
-  // The tansformation from the parent_link to the link_.
-  math::Pose pose_difference = link_->GetWorldCoGPose() - parent_links.at(0)->GetWorldCoGPose();
-  math::Vector3 drag_torque(0, 0, -turning_direction_ * force * moment_constant_);
-  // Transforming the drag torque into the parent frame to handle arbitrary rotor orientations.
-  math::Vector3 drag_torque_parent_frame = pose_difference.rot.RotateVector(drag_torque);
-  parent_links.at(0)->AddRelativeTorque(drag_torque_parent_frame);
+	// Forces from Philppe Martin's and Erwan Salaün's
+	// 2010 IEEE Conference on Robotics and Automation paper
+	// The True Role of Accelerometer Feedback in Quadrotor Control
+	// - \omega * \lambda_1 * V_A^{\perp}
+	ignition::math::Vector3d joint_axis = joint_->GetGlobalAxis(0).Ign();
+	ignition::math::Vector3d body_velocity_W = link_->GetWorldLinearVel().Ign();
+	ignition::math::Vector3d wind_speed_W = ignition::math::Vector3d::Zero;
+	ignition::math::Vector3d relative_wind_velocity_W = body_velocity_W - wind_speed_W;
+	ignition::math::Vector3d body_velocity_perpendicular =
+	  relative_wind_velocity_W -
+	  (relative_wind_velocity_W.Dot(joint_axis) * joint_axis);
+	ignition::math::Vector3d air_drag = -std::abs(real_motor_velocity) *
+						   rotor_drag_coefficient_ *
+						   body_velocity_perpendicular;
 
-  math::Vector3 rolling_moment;
-  // - \omega * \mu_1 * V_A^{\perp}
-  rolling_moment = -std::abs(real_motor_velocity) * rolling_moment_coefficient_ * body_velocity_perpendicular;
-  parent_links.at(0)->AddTorque(rolling_moment);
-  // Apply the filter on the motor's velocity.
-  double ref_motor_rot_vel;
-  ref_motor_rot_vel = rotor_velocity_filter_->updateFilter(ref_motor_rot_vel_, sampling_time_);
+	// Apply air_drag to link
+	link_->AddForce(air_drag);
+	// Moments get the parent link, such that the resulting torques can be
+	// applied.
+	physics::Link_V parent_links = link_->GetParentJointsLinks();
+	// The tansformation from the parent_link to the link_.
+	ignition::math::Pose3d pose_difference =
+	  (link_->GetWorldCoGPose() - parent_links.at(0)->GetWorldCoGPose()).Ign();
+	ignition::math::Vector3d drag_torque(
+	  0, 0, -turning_direction_ * thrust * moment_constant_);
+	// Transforming the drag torque into the parent frame to handle
+	// arbitrary rotor orientations.
+	ignition::math::Vector3d drag_torque_parent_frame =
+	  pose_difference.Rot().RotateVector(drag_torque);
+	parent_links.at(0)->AddRelativeTorque(drag_torque_parent_frame);
 
-#if 0 //FIXME: disable PID for now, it does not play nice with the PX4 CI system.
-  if (use_pid_)
-  {
-    double err = joint_->GetVelocity(0) - turning_direction_ * ref_motor_rot_vel / rotor_velocity_slowdown_sim_;
-    double rotorForce = pid_.Update(err, sampling_time_);
-    joint_->SetForce(0, rotorForce);
-    // gzerr << "rotor " << joint_->GetName() << " : " << rotorForce << "\n";
-  }
-  else
-  {
-#if GAZEBO_MAJOR_VERSION >= 7
-    // Not desirable to use SetVelocity for parts of a moving model
-    // impact on rest of the dynamic system is non-physical.
-    joint_->SetVelocity(0, turning_direction_ * ref_motor_rot_vel / rotor_velocity_slowdown_sim_);
-#elif GAZEBO_MAJOR_VERSION >= 6
-    // Not ideal as the approach could result in unrealistic impulses, and
-    // is only available in ODE
-    joint_->SetParam("fmax", 0, 2.0);
-    joint_->SetParam("vel", 0, turning_direction_ * ref_motor_rot_vel / rotor_velocity_slowdown_sim_);
-#endif
-  }
-#else
-  joint_->SetVelocity(0, turning_direction_ * ref_motor_rot_vel / rotor_velocity_slowdown_sim_);
-#endif /* if 0 */
+	ignition::math::Vector3d rolling_moment;
+	// - \omega * \mu_1 * V_A^{\perp}
+	rolling_moment = -std::abs(real_motor_velocity) *
+				   rolling_moment_coefficient_ *
+				   body_velocity_perpendicular;
+	parent_links.at(0)->AddTorque(rolling_moment);
+	// Apply the filter on the motor's velocity.
+	double ref_motor_rot_vel;
+	ref_motor_rot_vel = rotor_velocity_filter_->updateFilter(
+	  ref_motor_rot_vel_, sampling_time_);
+
+	// Make sure max force is set, as it may be reset to 0 by a world reset any
+	// time. (This cannot be done during Reset() because the change will be undone
+	// by the Joint's reset function afterwards.)
+	joint_->SetVelocity(
+	  0, turning_direction_ * ref_motor_rot_vel /
+	rotor_velocity_slowdown_sim_);
 }
 
 GZ_REGISTER_MODEL_PLUGIN(GazeboMotorModel);
